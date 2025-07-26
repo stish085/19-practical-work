f873a7ae7e6d3eb70fddb9f96601b83be51d1354
bestCoder <127156447+stish085@users.noreply.github.com>
2025-07-27 01:20:51 +0300

    Add files via upload

---
+++ 19 задание 3.cpp
@@ -0,0 +1,49 @@
+﻿// 19 задание 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main()
+{
+    std::cout << "Hello User !\n";
+    std::ifstream faile;
+    faile.open("C:\\practice\\statement.txt");
+
+    if (!faile.is_open())
+    {
+        std::cout << "file not open";
+        return 1;
+    }
+    
+        std::string surmame, name, answer, lastNameSurmame,data;
+        int payments=0, amountOfPayments = 0, cursor, bigPayments =0;
+        std::cout << "Faile : "<<std::endl;
+        while (!(faile.eof()))
+        {   
+           
+           
+           
+            faile >> name >> surmame >> payments >> data;
+            std::cout << name << " " << surmame << ": "  << payments<< std::endl;
+            std::cout << std::endl;
+            amountOfPayments += payments;
+            if (payments>bigPayments)
+            {   
+                bigPayments = payments;
+                answer = "";
+                answer = name + " " + surmame + ": " + std::to_string(payments);
+            }
+           
+
+        }
+        faile.close();
+        std::cout << "The sum of all payments: "<< amountOfPayments<<std::endl;
+        std::cout << std::endl;
+        std::cout << "made the largest payout: " << answer;
+        std::cout << std::endl;
+        return 0;
+
+}
+
